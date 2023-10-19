fe6e1abf7214a2269489fa7db4bffd1b0b6b52b6
DKUA-hub <58009568+DKUA-hub@users.noreply.github.com>
2023-10-19 10:45:44 +0300

    Update BaseClass.cs

--- BaseClass.cs
+++ BaseClass.cs
@@ -12,8 +12,8 @@ namespace RestSharpTest
 
         protected static IRestRequest RequestWithAuth(String endpoint) =>
             RequestWithoutAuth(endpoint)
-                .AddQueryParameter("key", "eb98438106f7eb8308aa3cb9d166457b")
-                .AddQueryParameter("token", "ATTAa74ad9c95079a0cb446a636c3e31fe3f62839f45a0976cb5bd8ce790cf7f46c5A03EDC4E");
+                .AddQueryParameter("key", "")
+                .AddQueryParameter("token", "");
 
         protected static IRestRequest RequestWithoutAuth(String endpoint) =>
             new RestRequest(endpoint);
